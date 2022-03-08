1c21d728f9bb5a61c2abac4568ea6607ebd83adc
KarlitosTaquitos <chern4261@gmail.com>
2022-03-08 10:15:43 -0600

    Listing teams in html page

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,1 +1,13 @@
-document.querySelector('h1').innerText="App";
+document.querySelector('h1').innerText = "App";
+
+fetch('/team').then(resp => resp.json()).then(teams => {
+    document.querySelector('#teams').innerHTML = listTeams(teams);
+});
+
+function listTeams(json) {
+    return `${json.map(listTeam).join('\n')}`;
+}
+
+function listTeam(team) {
+    return '<p>' + team.teamId + ': ' + team.teamName + '</p>';
+}
