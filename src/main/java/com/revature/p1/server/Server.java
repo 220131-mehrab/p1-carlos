14c152f1ea027be8ad4698574b7bb4dc4bd77334
KarlitosTaquitos <chern4261@gmail.com>
2022-03-09 15:34:07 -0600

    Cleaning up imports

--- src/main/java/com/revature/p1/server/Server.java
+++ src/main/java/com/revature/p1/server/Server.java
@@ -1,28 +1,14 @@
 package com.revature.p1.server;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.p1.domain.Team;
 import com.revature.p1.servlets.DefaultServlet;
 import com.revature.p1.servlets.TeamServlet;
 
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Server class to hold all Tomcat server methods
